2df41dd3b8e51d3e6146659219383c9eb7062aef
B͈̤̖̪̪̱ͅl̯̯̮̼͎̬͚̳̩̖̲u̜̼͉͈̠b͙̬̘̙̱̗̲͙b͍̞̬̬͓̼l̰̪͖̯̼̟̟͈̖͕̜̱̜ͅl̻̗͔̝̭̰͚͇̯̥ <34196146+blubbll@users.noreply.github.com>
2018-10-28 10:43:52 +0100

    Update node-helpers.js

--- node-helpers.js
+++ node-helpers.js
@@ -13,25 +13,3 @@ String.prototype.replaceAll = function(search, replacement) {
 //String minify
 String.prototype.minify=function(){var e=this;return e=(e=(e=(e=(e=(e=e.replace(/\/\*(?:(?!\*\/)[\s\S])*\*\/|[\r\n\t]+/g,""))
    .replace(/ {2,}/g," ")).replace(/ ([{:}]) /g,"$1")).replace(/([;,]) /g,"$1")).replace(/([{:}]) /g,"$1")).replace(/ !/g,"!")};
-
-//promise overwrite with catching error
-Promise.prototype.then = (oldThen=>{
-    return function then(_successHandler, _rejectHandler){
-        /* your logic here; 
-        remember: both successHandler and rejectHandler can be non-functions */
-        return oldThen.call(this, null, console.log('promise error'));
-    }
-})(Promise.prototype.then);
-class SubPromise extends Promise {
-    constructor(executor) {
-        super(function(_resolve, _reject) {
-           /* your code goes here */
-           return executor(null, console.log('promise error'));
-        });
-    }
-
-    then(success, reject) {
-        return super.then(null, console.log('promise error'));
-    }
-}
-Promise = SubPromise;
