221475fc805b90630e9d32c1f6b26ce797762dd6
B͈̤̖̪̪̱ͅl̯̯̮̼͎̬͚̳̩̖̲u̜̼͉͈̠b͙̬̘̙̱̗̲͙b͍̞̬̬͓̼l̰̪͖̯̼̟̟͈̖͕̜̱̜ͅl̻̗͔̝̭̰͚͇̯̥ <34196146+blubbll@users.noreply.github.com>
2018-11-01 17:13:07 +0100

    add info before loading

--- remquire.js
+++ remquire.js
@@ -1,4 +1,10 @@
-const remquire = async function(url,debug){
+const remquire = async function(url, debug) {
     return await fetch(url)
-    .then(function(t){return t.text()}).then(function(s){eval(s);if(debug)console.log(`imported & ran ${url}`)});
+        .then(function(t) {
+            return t.text()
+        }).then(function(s) {
+          if (debug) console.log(`importing ${url}...`)
+            eval(s);
+            if (debug) console.log(`imported & ran ${url}`)
+        });
 }
